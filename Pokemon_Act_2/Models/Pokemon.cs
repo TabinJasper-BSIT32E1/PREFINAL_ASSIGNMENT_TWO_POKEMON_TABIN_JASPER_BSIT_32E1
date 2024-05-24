70dc48c7428267ecb84f99b2c0af7b1216457915
TabinJasper-BSIT32E1 <Tabinjasper@gmail.com>
2024-05-24 14:38:55 +0800

    Added Model to handle class to represent a Pokemon with properties like name, moves and abilities

---
+++ Pokemon_Act_2/Models/Pokemon.cs
@@ -0,0 +1,52 @@
+﻿namespace Pokemon_Act_2.Models
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public List<PokemonMove> Moves { get; set; }
+        public List<AbilityEntry> Abilities { get; set; }
+    }
+    public class AbilityEntry
+    {
+        public Ability Ability { get; set; }
+        public bool IsHidden { get; set; }
+        public int Slot { get; set; }
+    }
+
+    public class Ability
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class PokemonMove
+    {
+        public Move Move { get; set; }
+        public List<VersionGroupDetail> VersionGroupDetails { get; set; }
+    }
+
+    public class Move
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class VersionGroupDetail
+    {
+        public int LevelLearnedAt { get; set; }
+        public MoveLearnMethod MoveLearnMethod { get; set; }
+        public VersionGroup VersionGroup { get; set; }
+    }
+
+    public class MoveLearnMethod
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class VersionGroup
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
