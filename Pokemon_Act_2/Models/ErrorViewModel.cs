228b2ae988762c5bc26073b2e2e3ee36bc7c921d
TabinJasper-BSIT32E1 <Tabinjasper@gmail.com>
2024-05-24 14:04:54 +0800

    Add project files.

---
+++ Pokemon_Act_2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pokemon_Act_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
